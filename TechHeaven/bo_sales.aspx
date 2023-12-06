f34a42dd2c81a38814d3200c412b825fb6fa54f5
Daniel Cunha <daniel.cunha.03@hotmail.com>
2023-12-06 00:31:23 +0000

    Sales without Pagination

---
+++ TechHeaven/bo_sales.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master_page_admin.Master" AutoEventWireup="true" CodeBehind="bo_sales.aspx.cs" Inherits="TechHeaven.bo_sales" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <main id="main" class="main">
+
+    <div class="pagetitle">
+        <h1>Encomendas</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index_admin.aspx">Inicio</a></li>
+                <li class="breadcrumb-item active">Encomendas</li>
+            </ol>
+        </nav>
+    </div>
+
+
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Encomendas</h5>
+
+
+
+                        <asp:Repeater ID="Repeater1" runat="server" OnItemDataBound="Repeater1_ItemDataBound">
+                            <ItemTemplate>
+                                <div class="accordion" id="accordionExample">
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id='<%# "heading" + Container.ItemIndex %>'>
+                                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target='<%# "#collapse" + Container.ItemIndex %>' aria-expanded="false" aria-controls='<%# "collapse" + Container.ItemIndex %>'>
+                                                Order #<%# Eval("id_order") %>
+                                            </button>
+
+                                        </h2>
+                                        <div id='<%# "collapse" + Container.ItemIndex %>' class="accordion-collapse collapse" aria-labelledby='<%# "heading" + Container.ItemIndex %>' data-bs-parent="#accordionExample">
+                                            <div class="accordion-body">
+                                                <p>ID: <%# Eval("id_order") %></p>
+                                                <p>Order Date: <%# Eval("order_date", "{0:MM/dd/yyyy}") %></p>
+                                                <p>Total: <%# Eval("total") %> €</p>
+                                                <p>Status: <%# Eval("status") %></p>
+                                                <p>Payment Method: <%# Eval("payment_methodID") %></p>
+
+                                                <h3>Products</h3>
+
+                                                <asp:Repeater ID="Repeater2" runat="server">
+                                                    <ItemTemplate>
+                                                        <p>
+                                                            Product Name: <%# Eval("name") %><br />
+                                                            Subtotal: <%# Eval("subtotal") %> €<br />
+                                                            Brand: <%# Eval("marca_nome") %>
+                                                        </p>
+                                                    </ItemTemplate>
+                                                </asp:Repeater>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </ItemTemplate>
+                        </asp:Repeater>
+                        <!-- End Default Accordion Example -->
+
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+</main>
+</asp:Content>
---
+++ TechHeaven/bo_sales.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TechHeaven
+{
+    public partial class bo_sales : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Fetch and display the list of orders when the page loads.
+                BindOrders();
+            }
+        }
+
+
+        private void BindOrders()
+        {
+            // Assuming you have a database connection string.
+            string connectionString = ConfigurationManager.ConnectionStrings["techeavenConnectionString"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Fetch the orders from your database, adjust the SQL query as per your database schema.
+                string query = "SELECT id_order, order_date, total, status, payment_methodID, addressID FROM orders";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable ordersTable = new DataTable();
+                    adapter.Fill(ordersTable);
+
+                    // Bind the orders to the Repeater1 control.
+                    Repeater1.DataSource = ordersTable;
+                    Repeater1.DataBind();
+                }
+            }
+        }
+
+        protected void Repeater1_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                // Get the order ID from the data item in the Repeater1 control.
+                DataRowView orderRow = e.Item.DataItem as DataRowView;
+                int orderID = (int)orderRow["id_order"];
+
+                // Find the Repeater2 control inside the current Repeater1 item.
+                Repeater Repeater2 = e.Item.FindControl("Repeater2") as Repeater;
+
+                // Call the DisplayProducts method to populate products for the current order.
+                DisplayProducts(orderID, Repeater2);
+            }
+        }
+
+        private void DisplayProducts(int id_encomenda, Repeater Repeater2)
+        {
+            // Assuming you have a database connection string.
+            string connectionString = ConfigurationManager.ConnectionStrings["techeavenConnectionString"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Modify the SQL query to select all products in the shopping cart for the specified order.
+                string query = "SELECT products.name, (products.price * cart.quantity) AS subtotal, brands.brand_name AS marca_nome " +
+                    "FROM cart " +
+                    "INNER JOIN products ON cart.productID = products.id_products " +
+                    "INNER JOIN brands ON products.brand = brands.id_brand " +
+                    "WHERE cart.orderID = @id_order";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id_order", id_encomenda);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable productsTable = new DataTable();
+                    adapter.Fill(productsTable);
+
+                    // Bind the product details to the Repeater2 control inside the accordion.
+                    Repeater2.DataSource = productsTable;
+                    Repeater2.DataBind();
+                }
+            }
+        }
+    }
+}
